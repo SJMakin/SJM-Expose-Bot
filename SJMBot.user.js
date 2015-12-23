b6901b1015eb6373c18dacf59328031a7110986b
SJMakin <SJMakin@users.noreply.github.com>
2015-12-23 15:32:00 +0000

    valid input

--- SJMBot.user.js
+++ SJMBot.user.js
@@ -14,86 +14,20 @@ image = new Image();
 image.crossOrigin = 'anonymous';
 image.src = 'http://i.imgur.com/V8EOXwT.png';
 window.agar.hooks.cellSkin = function(cell, old_skin) {
-    if (old_skin) return old_skin;
+    //if (old_skin) return old_skin;
     if (cell.isVirus) return image;
+    if (!cell.isVirus) return null;
     return null;
 }
+
+window.agar.hooks.cellColor = function(cell, old_color) {
+    //if (old_color) return old_color;
+    if (!cell.isVirus) return "#000000";
+    return old_color;
+}
+
 
 var boxRadius = 6;
-
-var nnInput = {[],[]};
-
-window.agar.hooks.afterDraw = function() {
-    var boxSize = boxRadius * 2 + 1;
-    var width = document.getElementById('canvas').width
-    var height = document.getElementById('canvas').height
-    var cellWidth = weight / boxSize;
-    var cellHeight = height / boxSize;
-    for (var a = 0; a < boxWidth; a++){
-        var 
-        for (var b = 0; b < boxWidth; b++){
-            
-        }
-    }
-    return null;
-}
-
-
-console.log('Loaded Virus Image Switch');
-
-//START - Helper methods
-// function compareSize(player1, player2, ratio) {
-//         if (player1.size * player1.size * ratio < player2.size * player2.size) {
-//             return true;
-//         }
-//         return false;
-//     }
-// 
-// function clusterFood(foodList, blobSize) {
-//         var clusters = [];
-//         var addedCluster = false;
-// 
-//         //1: x
-//         //2: y
-//         //3: size or value
-//         //4: Angle, not set here.
-// 
-//         for (var i = 0; i < foodList.length; i++) {
-//             for (var j = 0; j < clusters.length; j++) {
-//                 if (this.computeInexpensiveDistance(foodList[i][0], foodList[i][1], clusters[j][0], clusters[j][1]) < blobSize * 2) {
-//                     clusters[j][0] = (foodList[i][0] + clusters[j][0]) / 2;
-//                     clusters[j][1] = (foodList[i][1] + clusters[j][1]) / 2;
-//                     clusters[j][2] += foodList[i][2];
-//                     addedCluster = true;
-//                     break;
-//                 }
-//             }
-//             if (!addedCluster) {
-//                 clusters.push([foodList[i][0], foodList[i][1], foodList[i][2], 0]);
-//             }
-//             addedCluster = false;
-//         }
-//         return clusters;
-//     };
-//     
-function computeDistance(x1, y1, x2, y2, s1, s2) {
-        s1 = s1 || 0;
-        s2 = s2 || 0;
-        var xdis = x1 - x2; 
-        var ydis = y1 - y2;
-        var distance = Math.sqrt(xdis * xdis + ydis * ydis) - (s1 + s2);
-
-        return distance;
-}
-
-function isMe(cell){
-    for (var i = 0; i < window.agar.myCells.length; i++){            
-        if (window.agar.myCells[i] == cell.id){
-            return true;
-        }           
-    }
-    return false;
-}
 
 var transform = {scale:1, x:0, y:0} 
 window.agar.hooks.beforeTransform = (ctx, t1x, t1y, s, t2x, t2y) => { transform = {scale:s, x:t2x*s + t1x, y:t2y*s + t1y} } 
@@ -101,35 +35,165 @@ window.agar.hooks.beforeTransform = (ctx, t1x, t1y, s, t2x, t2y) => { transform 
 function toWorldCoords(pixelCoords) { 
     var x = (pixelCoords.x - transform.x) / transform.scale;
     var y = (pixelCoords.y - transform.y) / transform.scale;
-    console.log('PC x: ' + pixelCoords.x + ' PC y: ' + pixelCoords.y + ' WC x ' + x + ' WC y ' + y)
+    //console.log('PC x: ' + pixelCoords.x + ' PC y: ' + pixelCoords.y + ' WC x ' + x + ' WC y ' + y)
     return [x, y] 
 } 
     
 function toPixelCoords(worldCoords) { 
     var x = worldCoords.x*transform.scale + transform.x; 
     var y = worldCoords.y*transform.scale + transform.y;
-    console.log('WC x: ' + worldCoords.x + ' WC y: ' + worldCoords.y + ' PC x ' + x + ' PC y ' + y);
+    //console.log('WC x: ' + worldCoords.x + ' WC y: ' + worldCoords.y + ' PC x ' + x + ' PC y ' + y);
     return [x, y] 
 }
 
-function moveCell(x,y){
-   document.getElementById('canvas').onmousemove({clientX:x, clientY:y})
+// var nnInput = {[],[]};
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext("2d");
+var padding = 10
+var gridWidth = canvas.width - padding;
+var gridHeight = canvas.height - padding;
+
+window.agar.hooks.afterDraw = function() {
+    //drawLine(window.agar.dimensions[0],window.agar.dimensions[1],window.agar.dimensions[2],window.agar.dimensions[3]);
+    //drawCircle(window.agar.rawViewport.x, window.agar.rawViewport.y);
+    //drawCircle(player[k].x, player[k].y, player[k].size + this.splitDistance
+    // 
+    //top
+    // drawLine(window.agar.rawViewport.x - (gridWidth / 2) / transform.scale, window.agar.rawViewport.y - (gridHeight / 2) / transform.scale, window.agar.rawViewport.x + (gridWidth / 2) / transform.scale, window.agar.rawViewport.y - (gridHeight / 2) / transform.scale);
+    //bottom
+    // drawLine(window.agar.rawViewport.x - (gridWidth / 2) / transform.scale, window.agar.rawViewport.y + (gridHeight / 2) / transform.scale, window.agar.rawViewport.x + (gridWidth / 2) / transform.scale, window.agar.rawViewport.y + (gridHeight / 2) / transform.scale);
+    //left
+    //drawLine(window.agar.rawViewport.x - (gridWidth / 2) / transform.scale, window.agar.rawViewport.y - (gridHeight / 2) / transform.scale, window.agar.rawViewport.x - (gridWidth / 2) / transform.scale, window.agar.rawViewport.y + (gridHeight / 2) / transform.scale);
+    //right
+    //drawLine(window.agar.rawViewport.x + (gridWidth / 2) / transform.scale, window.agar.rawViewport.y - (gridHeight / 2) / transform.scale, window.agar.rawViewport.x + (gridWidth / 2) / transform.scale, window.agar.rawViewport.y + (gridHeight / 2) / transform.scale);
+
+    var boxSize = boxRadius * 2 + 1;
+
+    var cellWidth = gridWidth / boxSize;
+    var cellHeight = gridHeight / boxSize;
+    
+    var scanInterval = 30;
+    
+    var scanCount = 0;
+    //var inputs = [];
+    context.fillStyle="#FF0000";
+    context.font="20px Georgia";
+    for (var x = window.agar.rawViewport.x - (gridWidth / 2) / transform.scale; x <=window.agar.rawViewport.x + (gridWidth / 2) / transform.scale; x += cellWidth / transform.scale) {        
+        for (var y = window.agar.rawViewport.y - (gridHeight / 2) / transform.scale; y <= window.agar.rawViewport.y + (gridHeight / 2) / transform.scale; y += cellHeight / transform.scale) {
+            var cellThreatLevel = 0.0;
+            //for each interval within the cell get the color and add it to the total - we need a value between 1 and -1
+            //positive values are other players and virus, negative is food
+            //todo: color the cells greyscale accoring to theat level
+            for (var cellX = x; cellX < x + cellWidth; cellX += scanInterval){
+                for (var cellY = y; cellY < y + cellHeight; cellY += scanInterval){
+                    var pixelData = context.getImageData(cellX * transform.scale + transform.x, cellY * transform.scale + transform.y, 1, 1).data;
+                    cellThreatLevel += ((pixelData[0] + pixelData[1] + pixelData[2])/765 * -1) + 1;//255; //grey tone
+                    scanCount += 1;
+               }  
+            } 
+            cellThreatLevel = cellThreatLevel / scanCount; //mean
+            cellThreatLevel = +cellThreatLevel.toFixed(1)
+            context.fillText(cellThreatLevel, x, y)
+            //inputs.push(cellThreatLevel);
+            cellThreatLevel = 0;
+            scanCount = 0;
+        }
+    }    
+    
+    
+//     for (var x = window.agar.rawViewport.x - (gridWidth / 2) / transform.scale; x <=window.agar.rawViewport.x + (gridWidth / 2) / transform.scale; x += cellWidth / transform.scale) {
+//         context.moveTo(x, window.agar.rawViewport.y - (gridHeight / 2) / transform.scale);
+//         context.lineTo(x, window.agar.rawViewport.y + (gridHeight / 2) / transform.scale);
+//     }
+// 
+//     for (var x = window.agar.rawViewport.y - (gridHeight / 2) / transform.scale; x <= window.agar.rawViewport.y + (gridHeight / 2) / transform.scale; x += cellHeight / transform.scale) {
+//         context.moveTo(window.agar.rawViewport.x - (gridWidth / 2) / transform.scale, x);
+//         context.lineTo(window.agar.rawViewport.x + (gridWidth / 2) / transform.scale, x);
+//     }
+
+    context.strokeStyle = "black";
+    context.stroke();
+    
+    
+    
+    
+    
+    // 
+    // 
+    // for (var a = 0; a < boxWidth; a++){
+    //     var currentBoxWidth = cellWidth * (a + 1);
+    //     for (var b = 0; b < boxWidth; b++){
+    //         var currentBoxHeight = cellHeight * (b + 1);
+    //         for (var key in window.agar.allCells){
+    //             var cell = window.agar.allCells[key];
+    //             if ((cell.x < currentBoxWidth && cell.x > previousBoxWidth) && (cell.y < currentBoxHeight && cell.y > previousBoxHeight)){
+    //                 
+    //             }
+    //         }
+    //         previousBoxHeight = currentBoxHeight;
+    //     }
+    //     previousBoxWidth = currentBoxWidth;
+    // }
+    // return null;
 }
 
+// 
+// 
+// 
+// function compareSize(player1, player2) {
+//         return 1/(player1 / player2);
+// }
+// 
+// 
+// function computeDistance(x1, y1, x2, y2, s1, s2) {
+//         s1 = s1 || 0;
+//         s2 = s2 || 0;
+//         var xdis = x1 - x2; 
+//         var ydis = y1 - y2;
+//         var distance = Math.sqrt(xdis * xdis + ydis * ydis) - (s1 + s2);
+// 
+//         return distance;
+// }
+// 
+// function isMe(cell){
+//     for (var i = 0; i < window.agar.myCells.length; i++){            
+//         if (window.agar.myCells[i] == cell.id){
+//             return true;
+//         }           
+//     }
+//     return false;
+// }
+// 
+
+// 
+// function moveCell(x,y){
+//    document.getElementById('canvas').onmousemove({clientX:x, clientY:y})
+// }
+// 
+
+function drawCircle(x, y){
+      var radius = 70;
+
+      context.beginPath();
+      context.arc(x, y, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = 'green';
+      context.fill();
+      context.lineWidth = 5;
+      context.strokeStyle = '#003300';
+      context.stroke();
+}
 function drawLine(startX, startY, endX, endY){
-      var canvas = document.getElementById("canvas");
-      var context = canvas.getContext('2d');
 
       context.beginPath();
       context.moveTo(startX, startY);
       context.lineTo(endX, endY);
-      context.lineWidth = 4;
+      context.lineWidth = 3;
       context.stroke();
       
       //console.log('Line drawn');
       return true;
 }
-
+// 
 //END - Helper methods 
 
 //START - UI Customisation
@@ -138,51 +202,3 @@ window.agar.drawGrid = false;
 //END - UI Customisation
 
 //START - Bot control
-
-function processGameState(){
-    //console.debug("SJMBot processGameState");
-    var food = [];
-    var virus = [];
-    var me = [];
-    var other = [];
-    //console.dir(window.agar.allCells);
-    for (var key in window.agar.allCells){
-        var cell = window.agar.allCells[key];   
-        //console.log(cell.id);
-        if (isMe(cell)){
-            me.push(cell);
-        } else if (cell.size < 15) {
-            food.push(cell);
-        } else if (cell.isVirus){
-            virus.push(cell);           
-        } else {
-            other.push(cell);  
-        }
-    }
-    //console.log(me.length);
-    
-    if (me.length > 0 && food.length > 0){
-        var closest = 99999999;
-        var target = {};
-        for (var z = 0; z < food.length; z++){
-            var dist = computeDistance(me[0].x, me[0].y, food[z].x, food[z].y);
-            if (dist < closest){
-                closest = dist;
-                target = food[z];
-            }
-        }
-        //console.dir(me);
-        //console.dir(food);
-        var position = toPixelCoords(target);
-        moveCell(position[0], position[1]);
-        
-        //var myPosition = toPixelCoords(me[0]);
-        //drawLine (myPosition[0], myPosition[1], position[0], position[1]);
-    }
-
-}
-
-
-setInterval(processGameState, 30);
-//END - Bot control
-console.log("SJMBot loaded");
